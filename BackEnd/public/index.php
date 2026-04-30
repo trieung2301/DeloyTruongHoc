34eeefefa8463e6fda50afb3cf7944ac74f7ebf9
trieung2301 <118106842+trieung2301@users.noreply.github.com>
2026-04-30 16:13:15 +0700

    Cleanup project structure and add .dockerignore

--- BackEnd/public/index.php
+++ BackEnd/public/index.php
@@ -18,3 +18,6 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
